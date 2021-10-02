6453a4b0939c7385b948849b8abf82febc56f385
riflan <riflan0ahmed@gmail.com>
2021-10-02 10:56:35 +0530

    refactor: checking user

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -16,10 +16,9 @@ const Home = () => {
       </button>
       {isAuthenticated && (
         <div>
-          {/* <img src={user.picture} alt={user.name} /> */}
-          {/* <h2>{user.name}</h2> */}
-          {/* <p>{user.email}</p> */}
-          {user}
+          <img src={user?.picture} alt={user?.name} />
+          <h2>{user?.name}</h2>
+          <p>{user?.email}</p>
         </div>
       )}
     </>
