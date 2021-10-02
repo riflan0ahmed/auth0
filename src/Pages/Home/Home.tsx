850072f853355cf1bfd79731be65e7dcdfeb9412
riflan <riflan0ahmed@gmail.com>
2021-10-02 10:38:45 +0530

    refactor: added logout function

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,10 +1,13 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Home = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, logout } = useAuth0();
   return (
     <>
-      <button onClick={() => loginWithRedirect()}>Log In</button>;
+      <button onClick={() => loginWithRedirect()}>Log In</button>
+      <button onClick={() => logout({ returnTo: window.location.origin })}>
+        Log Out
+      </button>
     </>
   );
 };
