b4449b27da7cdfbe8afaa69a0eeaa1df1db21ad8
riflan <riflan0ahmed@gmail.com>
2021-10-04 06:46:14 +0530

    refactor: changed route pages

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,9 +1,17 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Home = () => {
-  const { logout } = useAuth0();
+  const { logout, user, isAuthenticated } = useAuth0();
   return (
     <>
+      {isAuthenticated && (
+        <div>
+          <img src={user?.picture} alt={user?.name} />
+          <h2>{user?.name}</h2>
+          <p>{user?.email}</p>
+        </div>
+      )}
+
       <button onClick={() => logout({ returnTo: window.location.origin })}>
         Log Out
       </button>
--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,9 @@ import { store } from "./store";
 import { Provider } from "react-redux";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+const domain = process.env.REACT_APP_DOMAIN;
+const clientId = process.env.REACT_APP_CLIENT_ID;
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
