e88ca5ba23ca34b727c5d63a53a21cb7c956ecb3
riflan <riflan0ahmed@gmail.com>
2021-10-04 06:35:59 +0530

    refactor: changed route pages

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,26 +1,12 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
-// interface User {
-//   name: string;
-//   picture: string;
-//   email: string;
-// }
-
 const Home = () => {
-  const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+  const { logout } = useAuth0();
   return (
     <>
-      <button onClick={() => loginWithRedirect()}>Log In</button>
       <button onClick={() => logout({ returnTo: window.location.origin })}>
         Log Out
       </button>
-      {isAuthenticated && (
-        <div>
-          <img src={user?.picture} alt={user?.name} />
-          <h2>{user?.name}</h2>
-          <p>{user?.email}</p>
-        </div>
-      )}
     </>
   );
 };
